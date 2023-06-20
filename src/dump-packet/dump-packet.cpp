0fc82116f81f47041acfa2eb04570b0817a0d5d9
Wangxy <xtwxy@hotmail.com>
2023-06-20 18:15:25 +0800

    add m-o-m mq proxy server.

--- src/dump-packet/dump-packet.cpp
+++ src/dump-packet/dump-packet.cpp
@@ -1,7 +1,234 @@
+//
+// async_tcp_echo_server.cpp
+// ~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2022 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <cstdlib>
 #include <iostream>
+#include <boost/bind/bind.hpp>
+#include <boost/asio.hpp>
 
+using boost::asio::ip::tcp;
 
-int main(int argc, char* argv[]) {
-  std::cout << "dump-packet not implemented." << std::endl;
+class mapper_session
+{
+public:
+  mapper_session(boost::asio::io_context& io_context)
+    : socket_(io_context)
+  {
+  }
+
+  tcp::socket& socket()
+  {
+    return socket_;
+  }
+
+  void start()
+  {
+    socket_.async_read_some(boost::asio::buffer(data_, max_length),
+        boost::bind(&mapper_session::handle_read, this,
+          boost::asio::placeholders::error,
+          boost::asio::placeholders::bytes_transferred));
+  }
+
+private:
+  void handle_read(const boost::system::error_code& error,
+      size_t bytes_transferred)
+  {
+    if (!error)
+    {
+      boost::asio::async_write(socket_,
+          boost::asio::buffer(data_, bytes_transferred),
+          boost::bind(&mapper_session::handle_write, this,
+            boost::asio::placeholders::error));
+    }
+    else
+    {
+      delete this;
+    }
+  }
+
+  void handle_write(const boost::system::error_code& error)
+  {
+    if (!error)
+    {
+      socket_.async_read_some(boost::asio::buffer(data_, max_length),
+          boost::bind(&mapper_session::handle_read, this,
+            boost::asio::placeholders::error,
+            boost::asio::placeholders::bytes_transferred));
+    }
+    else
+    {
+      delete this;
+    }
+  }
+
+  tcp::socket socket_;
+  enum { max_length = 1024 };
+  char data_[max_length];
+};
+
+class mapper_server
+{
+public:
+  mapper_server(boost::asio::io_context& io_context, short port)
+    : io_context_(io_context),
+      acceptor_(io_context, tcp::endpoint(tcp::v4(), port))
+  {
+    start_accept();
+  }
+
+private:
+  void start_accept()
+  {
+    mapper_session* new_mapper_session = new mapper_session(io_context_);
+    acceptor_.async_accept(new_mapper_session->socket(),
+        boost::bind(&mapper_server::handle_accept, this, new_mapper_session,
+          boost::asio::placeholders::error));
+  }
+
+  void handle_accept(mapper_session* new_mapper_session,
+      const boost::system::error_code& error)
+  {
+    if (!error)
+    {
+      new_mapper_session->start();
+    }
+    else
+    {
+      delete new_mapper_session;
+    }
+
+    start_accept();
+  }
+
+  boost::asio::io_context& io_context_;
+  tcp::acceptor acceptor_;
+};
+
+class mq_session
+{
+public:
+  mq_session(boost::asio::io_context& io_context)
+    : socket_(io_context)
+  {
+  }
+
+  tcp::socket& socket()
+  {
+    return socket_;
+  }
+
+  void start()
+  {
+    socket_.async_read_some(boost::asio::buffer(data_, max_length),
+        boost::bind(&mq_session::handle_read, this,
+          boost::asio::placeholders::error,
+          boost::asio::placeholders::bytes_transferred));
+  }
+
+private:
+  void handle_read(const boost::system::error_code& error,
+      size_t bytes_transferred)
+  {
+    if (!error)
+    {
+      boost::asio::async_write(socket_,
+          boost::asio::buffer(data_, bytes_transferred),
+          boost::bind(&mq_session::handle_write, this,
+            boost::asio::placeholders::error));
+    }
+    else
+    {
+      delete this;
+    }
+  }
+
+  void handle_write(const boost::system::error_code& error)
+  {
+    if (!error)
+    {
+      socket_.async_read_some(boost::asio::buffer(data_, max_length),
+          boost::bind(&mq_session::handle_read, this,
+            boost::asio::placeholders::error,
+            boost::asio::placeholders::bytes_transferred));
+    }
+    else
+    {
+      delete this;
+    }
+  }
+
+  tcp::socket socket_;
+  enum { max_length = 1024 };
+  char data_[max_length];
+};
+
+class mq_server
+{
+public:
+  mq_server(boost::asio::io_context& io_context, short port)
+    : io_context_(io_context),
+      acceptor_(io_context, tcp::endpoint(tcp::v4(), port))
+  {
+    start_accept();
+  }
+
+private:
+  void start_accept()
+  {
+    mq_session* new_mq_session = new mq_session(io_context_);
+    acceptor_.async_accept(new_mq_session->socket(),
+        boost::bind(&mq_server::handle_accept, this, new_mq_session,
+          boost::asio::placeholders::error));
+  }
+
+  void handle_accept(mq_session* new_mq_session,
+      const boost::system::error_code& error)
+  {
+    if (!error)
+    {
+      new_mq_session->start();
+    }
+    else
+    {
+      delete new_mq_session;
+    }
+
+    start_accept();
+  }
+
+  boost::asio::io_context& io_context_;
+  tcp::acceptor acceptor_;
+};
+
+int main(int argc, char* argv[])
+{
+  try
+  {
+    if (argc != 5)
+    {
+      std::cerr << "Usage: dump-packet <mapper port> <server port> <broker host> <broker port>\n";
+      return 1;
+    }
+
+    boost::asio::io_context io_context;
+
+    using namespace std; // For atoi.
+    mapper_server mapper(io_context, atoi(argv[1]));
+    mq_server mq(io_context, atoi(argv[2]));
+
+    io_context.run();
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "Exception: " << e.what() << "\n";
+  }
+
   return 0;
 }
